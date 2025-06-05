bfe7992d6b5183b0d9f4a09a6b660a60a5bc2a39
nandy-101 <nandhalal2003@gmail.com>
2025-06-05 21:29:09 +0530

    Project completed

---
+++ backend/src/lib/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const connectDB = async()=>{
+  try{
+    const conn= await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`Mongodb Connected: ${conn.connection.host}`);
+  }catch(error){
+    console.log("Couldnt connect",error);
+  }
+};
+
+export default connectDB;
